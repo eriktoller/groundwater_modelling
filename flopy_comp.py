c754393d3588ff8e0e5ccac6d4ec461b0528e2e8
eriktoller <erik@toller.se>
2026-02-19 13:38:41 +0100

    added exercise that maps half plane to first quadrant

--- flopy_comp.py
+++ flopy_comp.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     pi = np.pi
     qx = 10.0  # Flow in x-direction
     qy = 1.0  # Flow in y-direction
-    q = -500.0 # discharge (m^2/s)
-    r = 0.1 # radius of the well (m)
+    q0 = 10.0 # discharge (m^2/day)
+    q2 = 5.0 # discharge (m^2/day)
+    r = 0.25 # radius of the well (m)
     x0, y0 = 1.0, 2.0 # location of the first well
     x1, y1 = x0, -y0 # location of the second well
     x2, y2 = 2.0, 4.0 # location of the third well
@@ -29,7 +30,9 @@ if __name__ == "__main__":
         return omega
 
     # Define the functions for discharge potential and stream function
-    well = lambda x, y: well_phi(x, y, x0, y0, q, r) - well_phi(x, y, x1, y1, q, r) + well_phi(x, y, x2, y2, q, r) - well_phi(x, y, x3, y3, q, r)
+    phi0 = 1
+    well = lambda x, y: well_phi(x, y, x0, y0, q0, r) - well_phi(x, y, x1, y1, q0, r) + well_phi(x, y, x2, y2, q2, r) - well_phi(x, y, x3, y3, q2, r) + phi0
+    head0 = np.real(well(x0, y0+r))
     phi = lambda x, y: np.real(well(x, y))  # Discharge potential
     psi = lambda x, y: np.imag(well(x, y))  # Stream function
 
@@ -41,12 +44,11 @@ if __name__ == "__main__":
     yrange = (0, 10)
 
     # Generate contour plots
-    contour_flow_net(xrange, yrange, phi_func=phi, psi_func=psi, levels= 50, num_points=500)
+    contour_flow_net(xrange, yrange, phi_func=phi, psi_func=psi, levels= 20, num_points=500)
     plt.axis('equal') # Set equal scaling for both axes (this is important for flow nets)
 
     # Add labels and title
     plt.legend()
-    plt.title('Flow net for a well')
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
 
